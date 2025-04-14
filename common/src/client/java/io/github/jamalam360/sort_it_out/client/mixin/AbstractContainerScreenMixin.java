0a5f7b7f07087c44d3e60d0237ce9e7bbe7acffb
Jamalam <james@jamalam.tech>
2025-04-14 14:42:59 +0100

    fix: port 1.1.1

--- common/src/client/java/io/github/jamalam360/sort_it_out/client/mixin/AbstractContainerScreenMixin.java
+++ common/src/client/java/io/github/jamalam360/sort_it_out/client/mixin/AbstractContainerScreenMixin.java
@@ -8,12 +8,10 @@ import io.github.jamalam360.sort_it_out.client.gui.SortButton;
 import io.github.jamalam360.sort_it_out.network.BidirectionalUserPreferencesUpdatePacket;
 import io.github.jamalam360.sort_it_out.util.AbstractContainerMenuMixinImpl;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
-import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
@@ -71,64 +69,6 @@ public abstract class AbstractContainerScreenMixin extends Screen {
 	private void sort_it_out$checkForLeftOrTopPosChange(CallbackInfo ci) {
 		if (this.leftPos != this.sort_it_out$previousLeftPos || this.topPos != this.sort_it_out$previousTopPos) {
 			this.sort_it_out$initButtons();
-		}
-	}
-
-	@Inject(
-			method = "slotClicked",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;handleInventoryMouseClick(IIILnet/minecraft/world/inventory/ClickType;Lnet/minecraft/world/entity/player/Player;)V"
-			),
-			cancellable = true
-	)
-	private void sort_it_out$triggerSortOnMiddleClick(Slot slot, int slotId, int mouseButton, ClickType type, CallbackInfo ci) {
-		if (slotId < 0 || slotId >= this.menu.slots.size() || NetworkManager.canServerReceive(BidirectionalUserPreferencesUpdatePacket.C2S.TYPE)) {
-			return;
-		}
-
-		if (this.sort_it_out$impl.shouldSort(this.menu.getSlot(slotId), mouseButton, type, this.menu.getCarried(), Minecraft.getInstance().player)) {
-			SortItOutClient.sortOnEitherSide(this.menu, this.menu.getSlot(slotId));
-			ci.cancel();
-		}
-	}
-
-	@Unique
-	private void sort_it_out$initButtons() {
-		if ((Screen) this instanceof CreativeModeInventoryScreen) {
-			return;
-		}
-
-		List<SortButton> existingSortButtons = new ArrayList<>();
-		for (GuiEventListener widget : this.children()) {
-			if (widget instanceof SortButton sortButton) {
-				existingSortButtons.add(sortButton);
-			}
-		}
-
-		for (SortButton button : existingSortButtons) {
-			this.removeWidget(button);
-		}
-
-		List<ScreenSortButton> customButtons = ScreenSortButtonsLoader.INSTANCE.getCustomButtonsForScreen((AbstractContainerScreen<?>) (Object) this);
-		if (customButtons != null) {
-			for (ScreenSortButton button : customButtons) {
-				this.addRenderableWidget(new SortButton(this.leftPos + button.xOffset(), this.topPos + button.yOffset(), this.menu, this.menu.slots.get(button.slotStartIndex())));
-			}
-		} else {
-			// If custom sort buttons defs. are not present, at least guess where the player inventory sort button should be
-			Slot invSlot = null;
-
-			for (Slot slot : this.menu.slots) {
-				if (slot.container instanceof Inventory) {
-					invSlot = slot;
-					break;
-				}
-			}
-
-			if (invSlot != null) {
-				this.addRenderableWidget(new SortButton(this.leftPos + 158, this.topPos + this.imageHeight - 95, this.menu, invSlot));
-			}
 		}
 	}
 
