b8313da170ec12377a3f309bb4bfdb56b65f3568
Jamalam <james@jamalam.tech>
2025-04-14 14:55:54 +0100

    chore: backport to 1.20.1

--- common/src/client/java/io/github/jamalam360/sort_it_out/client/button/ScreenSortButtonsLoader.java
+++ common/src/client/java/io/github/jamalam360/sort_it_out/client/button/ScreenSortButtonsLoader.java
@@ -1,24 +1,28 @@
 package io.github.jamalam360.sort_it_out.client.button;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.JsonOps;
 import io.github.jamalam360.sort_it_out.SortItOut;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.FileToIdConverter;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class ScreenSortButtonsLoader extends SimpleJsonResourceReloadListener<ScreenSortButtons> {
+public class ScreenSortButtonsLoader extends SimpleJsonResourceReloadListener {
 	public static final ScreenSortButtonsLoader INSTANCE = new ScreenSortButtonsLoader();
 	private List<ScreenSortButtons> values;
 
 	private ScreenSortButtonsLoader() {
-		super(ScreenSortButtons.CODEC, FileToIdConverter.json("sort_buttons"));
+		super(new Gson(), "sort_buttons");
 	}
 
 	@Nullable
@@ -41,8 +45,19 @@ public class ScreenSortButtonsLoader extends SimpleJsonResourceReloadListener<Sc
 	}
 
 	@Override
-	protected void apply(Map<ResourceLocation, ScreenSortButtons> values, ResourceManager resourceManager, ProfilerFiller profiler) {
-		this.values = List.copyOf(values.values());
+	protected void apply(Map<ResourceLocation, JsonElement> values, ResourceManager resourceManager, ProfilerFiller profiler) {
+		this.values = new ArrayList<>();
+
+		for (Map.Entry<ResourceLocation, JsonElement> el : values.entrySet()) {
+			DataResult<ScreenSortButtons> result = ScreenSortButtons.CODEC.parse(JsonOps.INSTANCE, el.getValue());
+
+			if (result.error().isPresent()) {
+				SortItOut.LOGGER.error("Failed to decode screen sort button definition at {}: {}", el.getKey(), result.error().get().message());
+			} else {
+				this.values.add(result.get().orThrow());
+			}
+		}
+
 		SortItOut.LOGGER.info("Loaded {} sort button locations", values.size());
 	}
 }
