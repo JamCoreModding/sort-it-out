82c28530b144a58ffcfdd9c1cdcc4fa1f55bd8bf
Jamalam <james@jamalam.tech>
2026-01-26 13:25:20 +0000

    feat: respect creative mode tab order when sorting

--- common/src/main/java/io/github/jamalam360/sort_it_out/util/Comparators.java
+++ common/src/main/java/io/github/jamalam360/sort_it_out/util/Comparators.java
@@ -1,7 +1,6 @@
 package io.github.jamalam360.sort_it_out.util;
 
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.Comparator;
@@ -12,13 +11,5 @@ public class Comparators {
 	public static final Comparator<ItemStack> DURABILITY = Comparator.comparingInt(ItemStack::getDamageValue).reversed();
 	public static final Comparator<ItemStack> DISPLAY_NAME = Comparator.comparing((stack) -> stack.getDisplayName().getString());
 	public static final Comparator<ItemStack> NAMESPACE = Comparator.comparing((stack) -> BuiltInRegistries.ITEM.getKey(stack.getItem()).getNamespace());
-	public static final Comparator<ItemStack> CREATIVE_TAB = Comparator.comparing((stack) -> {
-		CreativeModeTab tab = CreativeModeTabLookup.INSTANCE.lookup(stack);
-
-		if (tab == null) {
-			return null;
-		} else {
-			return tab.getDisplayName().getString();
-		}
-	}, Comparator.nullsLast(String::compareTo));
+	public static final Comparator<ItemStack> CREATIVE_TAB = Comparator.comparing(CreativeModeTabLookup.INSTANCE::getOrder);
 }
