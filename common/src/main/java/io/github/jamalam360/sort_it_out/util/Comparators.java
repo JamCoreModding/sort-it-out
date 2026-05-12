b0427dc1b24ab403cfd2243a4de420fffeef619b
Jamalam <james@jamalam.tech>
2026-05-12 21:51:53 +0100

    fix: 1.20.1 compat

--- common/src/main/java/io/github/jamalam360/sort_it_out/util/Comparators.java
+++ common/src/main/java/io/github/jamalam360/sort_it_out/util/Comparators.java
@@ -1,12 +1,12 @@
 package io.github.jamalam360.sort_it_out.util;
 
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
+import java.awt.*;
 import java.util.Comparator;
-import java.util.Optional;
 
 public class Comparators {
 	public static final Comparator<ItemStack> EMPTINESS = Comparator.comparing(ItemStack::isEmpty);
@@ -14,6 +14,6 @@ public class Comparators {
 	public static final Comparator<ItemStack> CREATIVE_TAB = Comparator.comparing(CreativeModeTabLookup.INSTANCE::getOrder);
 	public static final Comparator<ItemStack> DISPLAY_NAME = Comparator.comparing((stack) -> stack.getDisplayName().getString());
 	public static final Comparator<ItemStack> DURABILITY = Comparator.comparingInt(ItemStack::getDamageValue).reversed();
-	public static final Comparator<ItemStack> ENCHANTMENTS = Comparator.comparing((stack) -> Optional.ofNullable(stack.get(stack.is(Items.ENCHANTED_BOOK) ? DataComponents.STORED_ENCHANTMENTS : DataComponents.ENCHANTMENTS)).flatMap(enchantments -> enchantments.keySet().stream().min(Comparator.comparing(holder -> holder.value().description().getString()))).map(holder -> holder.value().description().getString()).orElse(""));
+	public static final Comparator<ItemStack> ENCHANTMENTS = Comparator.comparing((stack) -> EnchantmentHelper.getEnchantments(stack).keySet().stream().map(e -> Component.translatable(e.getDescriptionId()).getString()).findFirst().orElse(""));
 	public static final Comparator<ItemStack> NAMESPACE = Comparator.comparing((stack) -> BuiltInRegistries.ITEM.getKey(stack.getItem()).getNamespace());
 }
