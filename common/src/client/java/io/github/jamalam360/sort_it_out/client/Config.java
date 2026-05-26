f1cb9ace651a0c6dcb57024584da4a03c0e55e0a
Jamalam <james@jamalam.tech>
2026-05-26 12:42:07 +0100

    fix: ensure minecraft is not null

--- common/src/client/java/io/github/jamalam360/sort_it_out/client/Config.java
+++ common/src/client/java/io/github/jamalam360/sort_it_out/client/Config.java
@@ -40,7 +40,7 @@ public class Config extends UserPreferences implements ConfigExtensions<Config> 
 	@Override
 	public void afterSave() {
 		// Only sync when in game
-		if (Minecraft.getInstance().level != null) {
+		if (Minecraft.getInstance() != null && Minecraft.getInstance().level != null) {
 			if (SortItOutClient.justReceivedFromServer) {
 				SortItOutClient.justReceivedFromServer = false;
 			} else {
